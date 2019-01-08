1d9205836a9a0539bd1cb1dde88e7aaac68c312a
Helder <heferreira@parceiro.sonae.pt>
2019-01-08 18:04:43 +0000

    working on the udemy tut

---
+++ section_7_components_communication/start/src/components/UserDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{switchName()}}</p>
+    <button>Switch the name assign in the parent from the children's component</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    }
+  },
+  methods: {
+    switchName() {
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  created() {
+    console.log("created()");
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
